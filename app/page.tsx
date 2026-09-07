1418ed9c9e6afea7f6b8971dc668042819651d42
EatoSystem <jason@eatosystem.com>
2026-09-07 12:41:56 +0100

    Remove legacy homepage from V2 preview

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,7 @@ import { cookies } from "next/headers";
 import { SiteHeader } from "@/components/layout/SiteHeader";
 import { SiteFooter } from "@/components/layout/SiteFooter";
 import { HomeHero } from "@/components/vascurra/home/hero";
-import { HumanContext } from "@/components/vascurra/home/human-context";
-import { FourPerspectives } from "@/components/vascurra/home/four-perspectives";
-import { UnderstandTrackSupport } from "@/components/vascurra/home/understand-track-support";
-import { Trust } from "@/components/vascurra/home/trust";
-import { Philosophy } from "@/components/marketing/Philosophy";
-import { ProjectStatus } from "@/components/marketing/ProjectStatus";
-import { FinalCta } from "@/components/vascurra/home/final-cta";
+import { Origin } from "@/components/vascurra/home/origin";
 import { HoldingUnlockForm } from "@/components/vascurra/forms/holding-unlock-form";
 import { HOLDING_COOKIE, isHoldingUnlocked } from "@/lib/holding-gate";
 
@@ -19,13 +13,7 @@ function FullHomePage() {
       <SiteHeader />
       <main id="main">
         <HomeHero />
-        <HumanContext />
-        <UnderstandTrackSupport />
-        <Philosophy />
-        <ProjectStatus />
-        <FourPerspectives />
-        <Trust />
-        <FinalCta />
+        <Origin />
       </main>
       <SiteFooter />
     </>
