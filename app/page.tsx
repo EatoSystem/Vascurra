27df74e485c56339f77157fdbfc155c7e91114af
EatoSystem <jason@eatosystem.com>
2026-09-07 13:16:20 +0100

    Keep holding page permanently on root

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,8 @@
-import { cookies } from "next/headers";
 import { SiteHeader } from "@/components/layout/SiteHeader";
-import { SiteFooter } from "@/components/layout/SiteFooter";
 import { HomeHero } from "@/components/vascurra/home/hero";
-import { Origin } from "@/components/vascurra/home/origin";
 import { HoldingUnlockForm } from "@/components/vascurra/forms/holding-unlock-form";
-import { HOLDING_COOKIE, isHoldingUnlocked } from "@/lib/holding-gate";
 
-function FullHomePage() {
-  return (
-    <>
-      <span id="top" />
-      <SiteHeader />
-      <main id="main">
-        <HomeHero />
-        <Origin />
-      </main>
-      <SiteFooter />
-    </>
-  );
-}
-
-function HoldingPage() {
+export default function HomePage() {
   return (
     <>
       <span id="top" />
@@ -31,12 +13,4 @@ function HoldingPage() {
       <HoldingUnlockForm />
     </>
   );
-}
-
-export default async function HomePage() {
-  const jar = await cookies();
-  if (await isHoldingUnlocked(jar.get(HOLDING_COOKIE)?.value)) {
-    return <FullHomePage />;
-  }
-  return <HoldingPage />;
 }
