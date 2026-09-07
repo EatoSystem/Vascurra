9cff82362f6b834f62dc74c2122786e2a9ff77ee
EatoSystem <jason@eatosystem.com>
2026-09-07 18:37:45 +0100

    Add Patient 0 section to gated V2 preview

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -5,6 +5,7 @@ import { SiteFooter } from "@/components/layout/SiteFooter";
 import { HomeHero } from "@/components/vascurra/home/hero";
 import { Origin } from "@/components/vascurra/home/origin";
 import { Mission } from "@/components/vascurra/home/mission";
+import { PatientZero } from "@/components/vascurra/home/patient-zero";
 import { HOLDING_COOKIE, isHoldingUnlocked } from "@/lib/holding-gate";
 
 export default async function PreviewPage() {
@@ -21,6 +22,7 @@ export default async function PreviewPage() {
         <HomeHero />
         <Origin />
         <Mission />
+        <PatientZero />
       </main>
       <SiteFooter />
     </>
