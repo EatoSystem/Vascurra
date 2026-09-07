5c39efc18f32aaf5f7e0b27c071e9028cd9a5731
EatoSystem <jason@eatosystem.com>
2026-09-07 13:30:12 +0100

    Add approved Section 02 to gated V2 preview

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import { SiteHeader } from "@/components/layout/SiteHeader";
 import { SiteFooter } from "@/components/layout/SiteFooter";
 import { HomeHero } from "@/components/vascurra/home/hero";
+import { Origin } from "@/components/vascurra/home/origin";
 import { HOLDING_COOKIE, isHoldingUnlocked } from "@/lib/holding-gate";
 
 export default async function PreviewPage() {
@@ -17,6 +18,7 @@ export default async function PreviewPage() {
       <SiteHeader />
       <main id="main">
         <HomeHero />
+        <Origin />
       </main>
       <SiteFooter />
     </>
